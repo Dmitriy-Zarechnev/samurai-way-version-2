9f6de88ace4100c8cec3cf4eeb7e6dab72516613
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-03-27 14:23:56 +0300

    27.03.2024 - lesson 18 - 3

--- src/components/sections/main/chat/ChatPage.tsx
+++ src/components/sections/main/chat/ChatPage.tsx
@@ -1,9 +1,6 @@
 import React, {memo, useEffect, useState} from 'react'
 
 
-// WebSocket соединение
-const ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
-
 // Response Type
 export type ChatMessageType = {
     message: string,
@@ -22,25 +19,81 @@ export const ChatPage = () => {
 
 
 export const Chat = () => {
+    const [wsChannel, setWsChannel] = useState<WebSocket | null>(null)
+    // // WebSocket соединение
+    // let wsChannel: WebSocket
+
+    useEffect(() => {
+        // Объявили WebSocket соединение
+        let ws: WebSocket
+
+        const closeEvent = () => {
+            // Рекурсивно вызвали себя же, чтоб создать соединение снова
+            // с определенным интервалом
+            setTimeout(() => {
+                createChannel()
+            }, 3000)
+        }
+
+        // Функция для добавления WebSocket соединения
+        function createChannel() {
+            // Отписались от старого события перед новой подпиской
+            ws?.removeEventListener('close', closeEvent)
+            // Принудительно закрыли WebSocket соединение
+            ws?.close()
+
+
+            // Инициализация WebSocket
+            ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+
+            // Подписка на потерю соединения
+            ws.addEventListener('close', closeEvent)
+
+            // Заменили значение в state
+            setWsChannel(ws)
+        }
+
+
+        createChannel()
+
+
+        return () => {
+            // Отписались от событий перед уходом со страницы
+            ws.removeEventListener('close', closeEvent)
+
+            // Принудительно закрыли WebSocket соединение
+            ws.close()
+        }
+    }, [])
 
 
     return (
         <div>
-            <ChatMessages/>
-            <ChatAddMessageForm/>
+            <ChatMessages wsChannel={wsChannel}/>
+            <ChatAddMessageForm wsChannel={wsChannel}/>
         </div>
     )
 }
 
 
-export const ChatMessages = () => {
+export const ChatMessages = (props: { wsChannel: WebSocket | null }) => {
     const [messages, setMessages] = useState<ChatMessageType[]>([])
 
+
     useEffect(() => {
-        ws.addEventListener('message', (e) => {
-            setMessages(JSON.parse(e.data))
-        })
-    }, [])
+        // Функция для обработчика
+        const messageEvent = (e: MessageEvent) => {
+            setMessages((prevMessages) => [...prevMessages, ...JSON.parse(e.data)])
+        }
+
+        // Подписались на новое сообщение
+        props.wsChannel?.addEventListener('message', messageEvent)
+
+        // Удалили обработчик после ухода с компоненты
+        return () => {
+            props.wsChannel?.removeEventListener('message', messageEvent)
+        }
+    }, [props.wsChannel])
 
     return (
         <div style={{height: '800px', overflowY: 'auto'}}>
@@ -69,11 +122,38 @@ export const ChatMessage = memo((props: { message: ChatMessageType }) => {
     )
 })
 
-export const ChatAddMessageForm = () => {
+export const ChatAddMessageForm = (props: { wsChannel: WebSocket | null }) => {
+    const [message, setMessage] = useState<string>('')
+    const [readyStatus, setReadyStatus] = useState<'pending' | 'ready'>('pending')
+
+    useEffect(() => {
+        // Функция для обработчика
+        const openEvent = () => {
+            setReadyStatus('ready')
+        }
+
+        // Подписались на изменение состояния канала
+        props.wsChannel?.addEventListener('open', openEvent)
+
+        // Удалили обработчик после ухода с компоненты
+        return () => {
+            props.wsChannel?.removeEventListener('open', openEvent)
+        }
+    }, [props.wsChannel])
+
+    const sendMessage = () => {
+        if (!message) {
+            return
+        }
+        props.wsChannel?.send(message)
+        setMessage('')
+    }
+
+
     return (
         <div>
-            <textarea></textarea>
-            <button>Send</button>
+            <textarea onChange={(e) => setMessage(e.currentTarget.value)} value={message}></textarea>
+            <button disabled={props.wsChannel === null || readyStatus !== 'ready'} onClick={sendMessage}>Send</button>
         </div>
     )
 }
